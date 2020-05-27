dfdbf8e0578356f5a0b4a4eddc3e18e0623198a1
B0ER <qwertygo85@gmail.com>
2020-05-27 22:41:53 +0300

    check declarations

--- src/apple-auth.d.ts
+++ src/apple-auth.d.ts
@@ -1,33 +1,16 @@
-export declare interface AppleAuthConfig {
-  client_id: string;
-  team_id: string;
-  redirect_uri: string;
-  key_id: string;
-  scope: string;
-}
-
-// https://developer.apple.com/documentation/signinwithapplerestapi/tokenresponse
-export declare interface AppleAuthAccessToken {
-  access_token: string;
-  expires_in: number;
-  id_token: string;
-  refresh_token: string;
-  token_type: "bearer";
-}
-
-// https://developer.apple.com/documentation/signinwithapplerestapi/errorresponse
-export declare interface AppleAuthError {
-  error: "invalid_request" | "invalid_client" | "invalid_grant" | "unauthorized_client" | "unsupported_grant_type" | "invalid_scope";
-}
-
-export declare type KeyMethod = 'file' | 'text';
+import { AppleAuthConfig, KeyMethod, AppleAuthAccessToken } from './interfaces';
+import { AppleClientSecret } from './token';
 
 declare class AppleAuth {
-  constructor(config: AppleAuthConfig, privateKeyLocation: string, privateKeyMethod: KeyMethod)
+  protected _config: AppleAuthConfig;
+  protected _state: string;
+  protected _tokenGenerator: AppleClientSecret;
   public readonly state: string;
+
+  constructor(config: AppleAuthConfig, privateKeyLocation: string, privateKeyMethod: KeyMethod)
   loginURL(): string;
   accessToken(code: string): Promise<AppleAuthAccessToken>;
-  refreshToken(code: string): Promise<AppleAuthAccessToken>;
+  refreshToken(refreshToken: string): Promise<AppleAuthAccessToken>;
 }
 
 export default AppleAuth;
